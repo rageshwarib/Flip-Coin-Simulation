930f6b4baef5dc5f2459652629b1d4114f79863c
rageshwari <ragu.bodke@gmail.com>
2020-03-23 16:33:38 +0530

    Storing Triplet combination in Dictionary and Determining it's percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -7,9 +7,18 @@ HH=0
 HT=0
 TH=0
 TT=0
+HHH=0
+TTT=0
+HTH=0
+HHT=0
+THH=0
+HTT=0
+THT=0
+TTH=0
 
 declare -A coinDictionarySinglet
 declare -A coinDictionaryDoublet
+declare -A coinDictionaryTriplet
 
 #Function to get Singlet Combination
 function getSingletCombination()
@@ -73,5 +82,57 @@ function getDoubletCombination()
 	echo "TT percentage ::" $TTpercentage
 }
 
+#function to get Triplet Combination
+function getTripletCombination()
+{
+	read -p "How many times do you want to flip a coin for Triplet Combination?:" NoOfFlips3
+	for((count=1;count<=$NoOfFlips3 * 3;count++))
+	do
+		outcome=$((RANDOM%8))
+		case $outcome in
+			0) coinDictionaryTriplet[HHH]=$((${coinDictionaryTriplet[HHH]}+1)) ;;
+			1) coinDictionaryTriplet[TTT]=$((${coinDictionaryTriplet[TTT]}+1)) ;;
+			2) coinDictionaryTriplet[HTH]=$((${coinDictionaryTriplet[HTH]}+1)) ;;
+			3) coinDictionaryTriplet[HHT]=$((${coinDictionaryTriplet[HHT]}+1)) ;;
+			4) coinDictionaryTriplet[THH]=$((${coinDictionaryTriplet[THH]}+1)) ;;
+			5) coinDictionaryTriplet[HTT]=$((${coinDictionaryTriplet[HTT]}+1)) ;;
+			6) coinDictionaryTriplet[THT]=$((${coinDictionaryTriplet[THT]}+1)) ;;
+			7) coinDictionaryTriplet[TTH]=$((${coinDictionaryTriplet[TTH]}+1)) ;;
+		esac
+	done
+
+	#Print the dictionary for Triplet Combination
+	for key in "${!coinDictionaryTriplet[@]}"
+	do
+		echo ""${key}" : "${coinDictionaryTriplet[$key]}""
+	done
+
+	#Calculating percentage of Triplet combination
+	HHHpercentage=`echo " scale = 4 ; (${coinDictionaryTriplet[HHH]}*100)/($NoOfFlips3*3)" | bc -l`
+	echo "HHH percentage ::" $HHHpercentage
+
+	TTTpercentage=`echo " scale = 4 ; (${coinDictionaryTriplet[TTT]}*100)/($NoOfFlips3*3)" | bc -l`
+	echo "TTT percentage ::" $TTTpercentage
+
+	HTHpercentage=`echo " scale = 4 ; (${coinDictionaryTriplet[HTH]}*100)/($NoOfFlips3*3)" | bc -l`
+	echo "HTH percentage ::" $HTHpercentage
+
+	HHTpercentage=`echo " scale = 4 ; (${coinDictionaryTriplet[HHT]}*100)/($NoOfFlips3*3)" | bc -l`
+	echo "HHT percentage ::" $HHTpercentage
+
+	THHpercentage=`echo " scale = 4 ; (${coinDictionaryTriplet[THH]}*100)/($NoOfFlips3*3)" | bc -l`
+	echo "THH percentage ::" $THHpercentage
+
+	HTTpercentage=`echo " scale = 4 ; (${coinDictionaryTriplet[HTT]}*100)/($NoOfFlips3*3)" | bc -l`
+	echo "HTT percentage ::" $HTTpercentage
+
+	THTpercentage=`echo " scale = 4 ; (${coinDictionaryTriplet[THT]}*100)/($NoOfFlips3*3)" | bc -l`
+	echo "THT percentage ::" $THTpercentage
+
+	TTHpercentage=`echo " scale = 4 ; (${coinDictionaryTriplet[TTH]}*100)/($NoOfFlips3*3)" | bc -l`
+	echo "TTH percentage ::" $TTHpercentage
+}
+
 getSingletCombination
 getDoubletCombination
+getTripletCombination
