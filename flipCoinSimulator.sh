4eca7ed33c9df24a1dc59c96f610ad03fb208dcb
rageshwari <ragu.bodke@gmail.com>
2020-03-23 16:25:53 +0530

    Storing Doublet combination in Dictionary and Determining it's percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,8 +3,13 @@
 #Variables
 H=0
 T=0
+HH=0
+HT=0
+TH=0
+TT=0
 
 declare -A coinDictionarySinglet
+declare -A coinDictionaryDoublet
 
 #Function to get Singlet Combination
 function getSingletCombination()
@@ -17,7 +22,7 @@ function getSingletCombination()
 		case $outcome in
 			0) coinDictionarySinglet[H]=$((${coinDictionarySinglet[H]}+1)) ;;
 			1) coinDictionarySinglet[T]=$((${coinDictionarySinglet[T]}+1)) ;;
-   	esac
+		esac
 	done
 
 	#Print the dictionary of singlet combination
@@ -32,4 +37,41 @@ function getSingletCombination()
 	echo "Percentage of heads ::" $headPercentage
 	echo "Percentage of tails ::" $tailPercentage
 }
+
+#Function to get Doublet Combination
+function getDoubletCombination()
+{
+	read -p "How many times do you want to flip a coin for doublet combination?:" NoOfFlips2
+	for((count=1;count<=$NoOfFlips2 * 2;count++))
+	do
+		outcome=$((RANDOM%4)) 
+		case $outcome in 
+			0) coinDictionaryDoublet[HH]=$((${coinDictionaryDoublet[HH]}+1)) ;;
+			1) coinDictionaryDoublet[HT]=$((${coinDictionaryDoublet[HT]}+1)) ;;
+			2) coinDictionaryDoublet[TH]=$((${coinDictionaryDoublet[TH]}+1)) ;;
+			3) coinDictionaryDoublet[TT]=$((${coinDictionaryDoublet[TT]}+1)) ;;
+		esac
+	done
+
+	# Print the dictionary for doublet combination
+	for key in "${!coinDictionaryDoublet[@]}"
+	do
+		echo ""${key}" : "${coinDictionaryDoublet[$key]}""
+	done
+
+	# Print the percentages of doublet combinations
+	HHpercentage=`echo " scale = 4 ; (${coinDictionaryDoublet[HH]}*100)/($NoOfFlips2*2)" | bc -l`
+	echo "HH percentage ::" $HHpercentage
+
+	HTpercentage=`echo " scale = 4 ; (${coinDictionaryDoublet[HT]}*100)/($NoOfFlips2*2)" | bc -l`
+	echo "HT percentahe ::" $HTpercentage
+
+	THpercentage=`echo " scale = 4 ; (${coinDictionaryDoublet[TH]}*100)/($NoOfFlips2*2)" | bc -l`
+	echo "TH percentage ::" $THpercentage
+
+	TTpercentage=`echo " scale = 4 ; (${coinDictionaryDoublet[TT]}*100)/($NoOfFlips2*2)" | bc -l`
+	echo "TT percentage ::" $TTpercentage
+}
+
 getSingletCombination
+getDoubletCombination
